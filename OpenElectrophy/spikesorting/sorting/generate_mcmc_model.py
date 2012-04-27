0ce26931deb83c655d8fdf350e0daeeb51c25147
Nicolas Fourcaud-Trocmé <nfourcau@neuro087.(none)>
2012-04-27 11:44:16 +0200

    MCMC: regroup all generate model in one file

--- OpenElectrophy/spikesorting/sorting/generate_mcmc_model.py
+++ OpenElectrophy/spikesorting/sorting/generate_mcmc_model.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 
 """
-**generate_mcmc_model**
+**generate_model with possible integration of information of time (ISI density) and possible adaptation of cluster weights**
 
-Generate a model in ND which should fit to the generated data.
-We make the hypotesis that our data are gaussians.
+Generate a model with the integration of the ISI density which should fit to the generated data.
 
 Inputs: 
-  * data is composed of all the unlabelled points.
+  * dict_data is composed of all the unlabelled points ('features' and 'time_vector').
   * nprior is the a priori number of elements in the mixture
-  * dim is the dimension of the data (only for generate_model)
+  * adapt_weights: if True, creates the 'weights' stochastic as a parent of 'labels'
+  * use_ISI: if True, creates S and sigmas variable to describe ISI as lognormal function for each cluster
   
-Outputs:
-  * covs are the parameter covariance matrix of the gaussians 
-  * thetas are the means of the gaussians
-  * labels is the labelization of the points
-  * mixture calculates the loglike probability 
-
+Outputs, dictionary of stochastics including keys:
+  * covs
+  * thetas
+  * labels
+  * mixture
+Optional keys:
+  * S
+  * sigmas
+  * weights
+  
+mixture is a stochastic variable which calculate the loglike probability.
+S contains the means of the distribution of the InterSpike Interval (ISI).
+sigmas contains the covariances of the distribution of the ISI.
 
 """
 
@@ -25,55 +32,94 @@ import pymc as pm
 import pymc.distributions as dist
 import numpy as np
 
-def generate_mcmc_model(data,nprior):	
+def generate_mcmc_model(dict_data,nprior,adapt_weights=False,use_ISI=False):	
     '''
     Model used for as dimension as you want.
-    generate_model(data,nprior,dim)
+    generate_model_isi(data,nprior,adapt_weights=False,use_ISI=False)
     Inputs: 
-      * data is composed of all the unlabelled points (row: points, columns: coordinates).
-      * nprior is the a priori number of elements in the mixture
-    We use the law InverseWishart to have a matrix symetric and positive define 
-    Outputs:
-      * covs is a vector containing the covariances of all the gaussians in the model
-      * thetas is a vector containing the means of all the gaussians in the model
-      * labels is a vector containing the labels of all the data
-      * mixture is a stochastic variable which calculate the loglike probability.
+        - dict_data is composed of all the unlabelled points (mandatory key: 'features', possible key: 'time_vector').
+        - nprior is the a priori number of elements in the mixture
+        - adapt_weights: if True, creates the 'weights' stochastic as a parent of 'labels'
+        - use_ISI: if True, creates S and sigmas variable to describe ISI as lognormal function for each cluster
+    Output, a dictionary of stochastic variables:
+        - covs is a vector containing the covariances of all the gaussians in the model
+        - thetas is a vector containing the means of all the gaussians in the model
+        - labels is a vector containing the labels of all the data
+        - mixture is a stochastic variable which calculate the loglike probability.
+    Optional dictionary keys:
+        - S is a vector containing all the means of the Interspike Interval (ISI) distributions.
+        - sigmas is a vector containing all the covariances of the ISI distributions.
+        - weights is a vector of weight on the labels
     '''
     
+    features=dict_data['features']
+    
     # Initializations
-    dim=data.shape[1]
-    sigma0 = np.diag([1., 1.])		
-    labels = []
+    prior_mean = features.mean(0)
+    sigma0 = np.diag([1., 1.])
+    c0 = np.cov(features.T)
     thetas = []
     covs = []
-    prior_mean = data.mean(0)
-    c0 = np.cov(data.T)
-    
-    for i in range(nprior): # for all the mixtures
+    sigmas = []
+    S = []
 
-        cov = pm.InverseWishart('C_%d' % i, n=4 , Tau=c0 ) # to get a  symetric and semidefinite positive matrix for the covariance
-        theta = pm.MvNormalCov('theta_%d' % i, mu=prior_mean, C=cov)
-        thetas.append(theta)
+    for j in range(nprior):# for all the mixtures
+    
+        cov = pm.InverseWishart('C_%d' % j, n=4 , Tau=c0 ) # to get a  symetric and semidefinite positive matrix for the covariance
+        theta = pm.MvNormalCov('theta_%d' % j, mu=prior_mean, C=cov) #theta is the mean
         covs.append(cov)
-    
-    alpha0 = 1.*np.ones(nprior) / nprior
-    labels=dist.Categorical('labels', p=alpha0,size=len(data))
+        thetas.append(theta)
+        
+        #ISI
+        if use_ISI:
+            sigma = pm.Uniform('sigma_%d' %j, lower = 0.1, upper = 3)
+            s = pm.Uniform('s_%d' %j, lower = 0.005, upper =3)
+            sigmas.append(sigma)
+            S.append(s)
 
-    @pm.stochastic(observed=True)
-    def mixture(value=data, thetas=thetas, covs=covs, labels=labels):
-        '''
-        Return the loglike value of the gaussians parameters 
-        '''
-        
-        loglike = 0.
-        for j, (theta, cov) in enumerate(zip(thetas, covs)):
-            this_data=data[labels==j]
-            ch = np.linalg.cholesky(cov)# to get a triangular matrix for the covariance
-            loglike += pm.mv_normal_chol_like(this_data, theta, ch) 
-        
-        return loglike
-        
-    dict_stochastics={'covs':covs,'thetas':thetas,'labels':labels,'mixture':mixture}
+    alpha0 = 1.*np.ones(nprior) / nprior
+    if adapt_weights:
+        weights = pm.Dirichlet('weights', theta=alpha0)
+        labels=dist.Categorical('labels', p=weights,size=features.shape[0])
+    else:
+        labels=dist.Categorical('labels', p=alpha0,size=features.shape[0])
     
+    @pm.stochastic(observed=True)
+    def mixture(value = 1, thetas = thetas, covs = covs, labels = labels,sigmas = sigmas, S = S):
+        '''
+        Return the loglike value of the gaussians parameters and of the parameters of the ISI distributions
+        '''		
+        loglike = 0.
+        
+        for j, (theta, cov) in enumerate(zip(thetas, covs)):
+            
+            cluster_points = labels == j
+            this_features = features[cluster_points]
+
+            ch = np.linalg.cholesky(cov)
+            loglike += pm.mv_normal_chol_like(this_features, theta, ch)
+
+            if sigmas is not None:
+                sigma = sigmas[j]
+                s = S[j]           
+                this_time = dict_data['time_vector'][cluster_points]
+                isi = np.diff(this_time)
+                loglike += pm.lognormal_like(isi, mu = s, tau = sigma)
+
+        return loglike
+            
+    dict_stochastics={'covs':covs,'thetas':thetas,'labels':labels,'mixture':mixture}
+
+    if adapt_weights:
+        dict_stochastics['weights']=weights
+
+    if use_ISI:
+        dict_stochastics['S']=S
+        dict_stochastics['sigmas']=sigmas
+    else:
+        S=None
+        sigmas=None
+
+
     return dict_stochastics
 
--- OpenElectrophy/spikesorting/sorting/gmm_mcmc.py
+++ OpenElectrophy/spikesorting/sorting/gmm_mcmc.py
@@ -3,11 +3,7 @@ import quantities as pq
 import numpy as np
 import pymc as pm
 import MetropolisCategorical
-from generate_mcmc_model import generate_mcmc_model
-from generate_mcmc_model_weights import generate_mcmc_model_weights
-from generate_mcmc_model_isi import generate_mcmc_model_isi
-from generate_mcmc_model_weights_isi import generate_mcmc_model_weights_isi
-
+import generate_mcmc_model as gen
 
 class GaussianMixtureMCMC(object):
     """
@@ -21,40 +17,36 @@ class GaussianMixtureMCMC(object):
         n_burn: number of iteration rejected (assuming convergence is not reached)
         thin: number of step between two recordings of sampled values
         adapt_cluster_weights: if True (default) favors the creation of empty clusters and thus try to find the best number
-            of cluster >= n_cluster
-        include_isi: if True (default) takes into account isi distribution of clusters by fitting it to a lognormal distribution 
+            of cluster ( <=n_cluster )
+        use_ISI: if True (default) takes into account ISI distribution of clusters by fitting it to a lognormal distribution 
             (in particular it avoids short ISIs)
+        nb_draw: number of label draws between two other stochastic variable draws (Gaussian or ISI parameters for example)
     
     """
     
     name = 'GMM MCMC'
     
-    def run(self, spikesorter, n_cluster = 1, n_iter = 800, n_burn=500, thin=1,verbose=0,adapt_cluster_weights=False,include_isi=True):
+    def run(self, spikesorter, n_cluster = 1, n_iter = 800, n_burn=500, thin=1,verbose=0,adapt_cluster_weights=True,use_ISI=True,nb_draw=40):
         
         sps = spikesorter
-        
-        # Choose the correct model:
-        if include_isi:
+
+        dict_data={'features':sps.waveform_features}
+
+        if use_ISI:
             time_vector=np.empty(sps.waveform_features.shape[0])
             time_shift=0.*pq.s
             for s, ind in enumerate(sps.spike_index_array):
                 time_vector[sps.seg_spike_slices[s]]=1.*ind/((sps.sig_sampling_rate).simplified)+time_shift
                 time_shift=(time_vector[sps.seg_spike_slices[s]][-1]+1.)*pq.s # add one empty second between segments
                 # this may cause small problems to classify spikes close to the segment edges
-            dict_data={'features':sps.waveform_features,'time_vector':time_vector}
-            if adapt_cluster_weights:
-                dict_stochastics=generate_mcmc_model_weight_isi(dict_data,n_cluster)
-            else:
-                dict_stochastics=generate_mcmc_model_isi(dict_data,n_cluster)
-        else:
-            if adapt_cluster_weights:
-                dict_stochastics=generate_mcmc_model_weight(sps.waveform_features,n_cluster)
-            else:
-                dict_stochastics=generate_mcmc_model(sps.waveform_features,n_cluster)
+            dict_data['time_vector']=time_vector
+        
+        # Choose the model
+        dict_stochastics=gen.generate_mcmc_model(dict_data,n_cluster,adapt_weights=adapt_cluster_weights,use_ISI=use_ISI)
         
         #Creation of the model
         sampler = pm.MCMC(dict_stochastics.values())
-        sampler.use_step_method(MetropolisCategorical.MetropolisCategorical,dict_stochastics['labels'],nb_gauss=n_cluster, nb_draw = 40) 
+        sampler.use_step_method(MetropolisCategorical.MetropolisCategorical,dict_stochastics['labels'],nb_gauss=n_cluster, nb_draw = nb_draw) 
         sampler.sample(iter=n_iter,burn=n_burn,thin=thin,verbose=verbose)
         
         sps.spike_clusters=sampler.trace('labels')[-1]
