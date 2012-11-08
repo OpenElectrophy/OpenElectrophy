ed615da6259c6d20cfb0654376ba1059e0ac4692
Samuel Garcia <sam.garcia.die@gmail.com>
2012-11-08 12:02:24 +0100

    yep

---
+++ test draft/gui/viewers/create_segment.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import quantities as pq
+import neo
+
+nb_sig = 4
+
+#~ sig_size = 3.6e8
+#~ sig_size = 1e7
+sig_size = 1e6
+nb_spike =  10e3
+#~ nb_spike =  0
+fs = 10.e3
+t = np.arange(sig_size)/fs
+t_start = -5.
+
+analogsignals = [ ]
+spiketrains_on_signals = [ ]
+for i in range(nb_sig):
+    sig = 2*np.sin(t*np.pi*2*25.) + np.random.randn(sig_size)+i*10.
+    spikepos = np.random.randint(sig.size, size =nb_spike)
+    sig[spikepos] += 15
+    analogsignals.append(neo.AnalogSignal(sig, units = 'uV', t_start=t_start*pq.s, sampling_rate = fs*pq.Hz))
+    spiketrains_on_signals.append([ ])
+    for i in range(2):
+        color = ['magenta', 'green', 'blue', 'red'][i%4]
+        spiketrains_on_signals[-1].append(neo.SpikeTrain(spikepos[i*nb_spike/2:(i+1)*nb_spike/2]/fs+t_start,
+                    t_start = t_start, t_stop = t_start+sig_size/fs, units = 's', color = color))
+
+
+
+seg = neo.Segment(name = 'test')
+
+seg.analogsignals = analogsignals
+#~ seg.spiketrains = 
