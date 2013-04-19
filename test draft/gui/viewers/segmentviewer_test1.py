f1350fc7c5d7673e1828e843bda60a1e21c7f359
Samuel Garcia <sgarcia@sgarcia-DellXPS13>
2013-04-19 22:53:42 +0200

    Some viewer debug.

--- test draft/gui/viewers/segmentviewer_test1.py
+++ test draft/gui/viewers/segmentviewer_test1.py
@@ -19,5 +19,17 @@ def test1():
     app.exec_()
 
 
+def test2():
+    #~ seg = neo.Spike2IO(filename = 'Rat9_11_08_21_10-00-00.SMR').read_segment()
+    seg = neo.Spike2IO(filename = 'R28-C4-B.SMR').read_segment()
+    #~ seg = neo.MicromedIO(filename = 'EEG_1503.TRC').read_segment()
+    
+    app = QApplication([ ])
+    w1 = SegmentViewer(segment = seg, xsize = 15)
+    w1.show()
+    app.exec_()
+
+
 if __name__ == '__main__' :
-    test1()
+    #~ test1()
+    test2()
