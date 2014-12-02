65efef78296d2ce22833134208d56b2a6fca0268
Samuel Garcia <sam.garcia.die@gmail.com>
2014-12-02 20:57:03 +0100

    NLS1 NLS2

--- OpenElectrophy/io/__init__.py
+++ OpenElectrophy/io/__init__.py
@@ -6,6 +6,7 @@ Add some more IOs than neo.io
 import neo
 
 from .tryitio import TryItIO
+from .neurolabscope2 import Neurolabscope2IO
+from .neurolabscope1 import Neurolabscope1IO
 
-
-iolist = [ TryItIO ] + neo.io.iolist
+iolist = [ TryItIO, Neurolabscope2IO, Neurolabscope1IO] + neo.io.iolist
